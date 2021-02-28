04258a8aef97bf77007a6f2415ffb988bd05db37
Patrick Jamieson <patrick@cleancodecorp.com>
2021-02-28 15:50:09 -0800

    Initial code commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a489ec4bb44bb397918d2c1c6b462ae2'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3f4adc9a-cf34-49a1-aa4e-265e8f6f65f1'
+};
