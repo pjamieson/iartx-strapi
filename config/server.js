317656ecd41d5c2f4267b5bdbcc375178f8808a0
Patrick Jamieson <patrick@cleancodecorp.com>
2021-03-01 10:05:50 -0800

    Updates server config

--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'https://api.iartx.com',
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'a489ec4bb44bb397918d2c1c6b462ae2'),
+      secret: env('ADMIN_JWT_SECRET'),
     },
   },
 });
