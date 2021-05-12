9f1e183ec56d24e42a9322c5391df5a613204ab0
Patrick Jamieson <patrick@cleancodecorp.com>
2021-05-12 13:18:13 -0700

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  //url: 'https://api.iartx.com',
+  url: 'https://api.iartx.com',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET'),
